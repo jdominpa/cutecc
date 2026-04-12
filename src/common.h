c9bfc78a6b8db53b0884ce0370ec5a9a78d796f1
Joan Domingo Pasarin <work@jdompas.com>
2026-04-12 16:07:24 +0200

    minor fix to MIN and MAX macros

--- src/common.h
+++ src/common.h
@@ -16,7 +16,7 @@
         abort();                                                           \
     } while (0)
 
-#define MAX(a, b) a > b ? a : b
-#define MIN(a, b) a > b ? b : a
+#define MAX(a, b) (a) > (b) ? (a) : (b)
+#define MIN(a, b) (a) > (b) ? (b) : (a)
 
 #endif  // COMMON_H_
