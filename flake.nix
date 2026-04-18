5f57b8af4c4f73cc2986831ae8142d1d14059130
Joan Domingo Pasarin <work@jdompas.com>
2026-04-18 14:28:09 +0200

    add flake for nixos development

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+
+  outputs = { self, ... }@inputs:
+  let
+    supportedSystems = [ "x86_64-linux" ];
+    forEachSupportedSystem = f:
+      inputs.nixpkgs.lib.genAttrs supportedSystems (
+        system: f {
+          inherit system;
+          pkgs = import inputs.nixpkgs { inherit system; };
+        }
+      );
+  in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs, system }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              clang-tools
+              cmake
+              gcc
+              gdb
+              self.formatter.${system}
+            ];
+          };
+        }
+      );
+
+      formatter = forEachSupportedSystem ({ pkgs, ... }: pkgs.nixfmt);
+    };
+}
