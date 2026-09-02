a9cea7b27d43802068817963f82f1feec7cf1e62
Joan Domingo Pasarin <work@jdompas.com>
2026-09-02 22:42:39 +0200

    chore: add lldb debugserver path as an env var on macos

--- devenv.nix
+++ devenv.nix
@@ -1,8 +1,11 @@
 {
+  lib,
   pkgs,
   ...
 }:
 {
   languages.c.enable = true;
   packages = [ pkgs.lldb ];
+  env.LLDB_DEBUGSERVER_PATH =
+    lib.mkIf pkgs.stdenv.hostPlatform.isDarwin "/Library/Developer/CommandLineTools/Library/PrivateFrameworks/LLDB.framework/Resources/debugserver";
 }
